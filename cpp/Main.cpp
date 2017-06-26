d9e091782d588a404c03b603b39489af9a0e7f77
dmaul <jesus.jz@gmail.com>
2017-06-26 16:58:53 -0300

    inheritance

--- cpp/Main.cpp
+++ cpp/Main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include "impl/classes/all.hpp"
 
 int main() {
 
   MainCpp instance;
+  PosixMainCpp posixInstance;
 
   instance.printMsg();
-
+  posixInstance.mapFileToM();
+ 
 }
--- cpp/impl/classes/MainClass.cpp
+++ cpp/impl/classes/MainClass.cpp
@@ -1,12 +1,21 @@
-#include <iostream>
-
 using namespace std;
 
 class MainCpp {
     public:
         void printMsg();
+        virtual int mapFileToM(){return 0;};
 };
 
 void MainCpp::printMsg() {
     cout << "Just a message" << endl;
 }
+
+
+class PosixMainCpp: public MainCpp {
+    public:
+      virtual int mapFileToM() {
+        cout << "mapping file to memory" << endl;
+        return 0;
+      }
+};
+
